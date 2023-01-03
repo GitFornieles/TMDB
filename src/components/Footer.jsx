a93b5d365e62c67642dc21dd9d8f86f0e3d34219
GitFornieles <alefornieles@hotmail.com>
2023-01-03 19:56:54 -0300

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,31 @@
-const Footer=()=>{
-    return <footer>
-        <h4>FOOTER</h4>
-    </footer>
-}
+import {FaLinkedinIn,FaGithub,FaIdBadge} from "react-icons/fa"
+import {MdAlternateEmail} from "react-icons/md"
 
-export default Footer
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <a class="navButton" href="https://github.com/GitFornieles">
+        <FaGithub/>
+        </a>
+      </div>
+      <div>
+        <a class="navButton" href="https://www.linkedin.com/in/afornieles/">
+        <FaLinkedinIn/>
+        </a>
+      </div>
+      <div>
+        <a class="navButton" href="https://www.linkedin.com/in/afornieles/">
+            <FaIdBadge/>
+        </a>
+      </div>
+      <div>
+      <a class="navButton" href="mailto:alefornieles@hotmail.com">
+            <MdAlternateEmail/>
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
